a70f41194288da2ba37fb4c8ad666b685f7777a6
Junaid Syed <sjunaid626@gmail.com>
2023-10-26 20:45:26 +0530

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,7 +4,7 @@ import { RiMailSendLine } from 'react-icons/ri';
 import { TbBrandGithubFilled } from 'react-icons/tb';
 
 const Homepage = () => (
-  <section className="bg-home-bg-mobile bg-white bg-no-repeat bg-cover flex flex-col items-center justify-center px-8 sm:p-48  sm:bg-home-bg-desktop md:w-full md:bg-contain md:bg-no-repeat rounded-bl-[100px] bg-center h-screen">
+  <section className="bg-home-bg-mobile bg-white bg-no-repeat bg-contain flex flex-col items-center justify-center px-8 sm:p-48  sm:bg-home-bg-desktop md:w-full md:bg-contain md:bg-no-repeat rounded-bl-[100px] bg-center h-screen">
     <div className="gap-4 flex flex-col">
       <h1 className="text-primary font-bold text-40 leading-52">
         I&apos;m Junaid.
@@ -25,11 +25,11 @@ const Homepage = () => (
         LET&apos;S CONNECT
       </p>
       <div className="text-quaternary flex gap-2">
-        <a href="https://twitter.com/junaidahmedsd" aria-label="Twitter"><FaTwitter className="hover:text-sky-400 hover:cursor-pointer text-xl" /></a>
-        <a href="https://www.linkedin.com/in/junaidahmedsyed/" aria-label="LinkedIn"><FaLinkedin className="hover:text-[#0a66c2] hover:cursor-pointer text-xl" /></a>
-        <a href="mailto:sjunaid626@gmail.com" aria-label="Mail"><RiMailSendLine className="hover:text-red-400 hover:cursor-pointer text-xl" /></a>
-        <a href="https://github.com/iamsjunaid" aria-label="GitHub"><TbBrandGithubFilled className="hover:text-black hover:cursor-pointer text-xl" /></a>
-        <a href="https://wellfound.com/u/junaid-ahmed-shakeel-ahmed-syed" aria-label="Wellfound"><FaAngellist className="hover:text-black hover:cursor-pointer text-xl" /></a>
+        <a href="https://twitter.com/junaidahmedsd" aria-label="Twitter"><FaTwitter className="hover:text-sky-400 hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" /></a>
+        <a href="https://www.linkedin.com/in/junaidahmedsyed/" aria-label="LinkedIn"><FaLinkedin className="hover:text-[#0a66c2] hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" /></a>
+        <a href="mailto:sjunaid626@gmail.com" aria-label="Mail"><RiMailSendLine className="hover:text-red-400 hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" /></a>
+        <a href="https://github.com/iamsjunaid" aria-label="GitHub"><TbBrandGithubFilled className="hover:text-black hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" /></a>
+        <a href="https://wellfound.com/u/junaid-ahmed-shakeel-ahmed-syed" aria-label="Wellfound"><FaAngellist className="hover:text-black hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" /></a>
       </div>
     </div>
   </section>
