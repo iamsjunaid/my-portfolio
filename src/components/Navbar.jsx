ad09b5b1f3cd4caae166097cdd4d9b2ad90bae58
Junaid Syed <sjunaid626@gmail.com>
2023-10-27 16:57:34 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import '../assets/styles/Navbar.css';
+import { AiFillHome } from 'react-icons/ai';
+import { FaUser } from 'react-icons/fa';
+import { BsChatSquareTextFill } from 'react-icons/bs';
+import { SiBookstack } from 'react-icons/si';
+import logo from '../assets/img/logo.png';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="flex justify-between sticky top-0 shadow-md bg-white z-50">
-      <div className="flex justify-between items-center p-4 w-full">
-        <Link to="/" className="font-bold text-2xl text-secondary transition duration-500 ease-in-out">
-          Logo
-        </Link>
+    <nav className="flex justify-between sticky top-0 shadow-md bg-white z-50 px-4">
+      <div className="flex justify-between items-center w-full">
+        <a href="#home" className="text-secondary transition duration-500 ease-in-out">
+          <img src={logo} alt="logo" className="w-24" />
+        </a>
         <GiHamburgerMenu
           className="menu text-2xl text-secondary cursor-pointer transition duration-500 ease-in-out"
           onClick={() => setMenuOpen(!menuOpen)}
@@ -19,16 +23,28 @@ const Navbar = () => {
       </div>
       <ul className={menuOpen ? 'open' : ''}>
         <li>
-          <NavLink to="/" className="hover:text-secondary text-xl transition duration-500 ease-in-out">Home</NavLink>
+          <a href="#home" className="flex items-center text-quaternary hover:text-secondary text-xl transition duration-500 ease-in-out">
+            <AiFillHome className="text-xl" />
+            Home
+          </a>
         </li>
         <li>
-          <NavLink to="/portfolio" className="hover:text-secondary text-xl transition duration-500 ease-in-out">Portfolio</NavLink>
+          <a href="#portfolio" className="flex items-center text-quaternary hover:text-secondary text-xl transition duration-500 ease-in-out">
+            <SiBookstack className="text-xl" />
+            Portfolio
+          </a>
         </li>
         <li>
-          <NavLink to="/about" className="hover:text-secondary text-xl transition duration-500 ease-in-out">About</NavLink>
+          <a href="#about" className="flex items-center text-quaternary hover:text-secondary text-xl transition duration-500 ease-in-out">
+            <FaUser className="text-xl" />
+            About
+          </a>
         </li>
         <li>
-          <NavLink to="/contact" className="hover:text-secondary text-xl transition duration-500 ease-in-out">Contact</NavLink>
+          <a href="#contact" className="flex items-center text-quaternary hover:text-secondary text-xl transition duration-500 ease-in-out">
+            <BsChatSquareTextFill className="text-xl" />
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
