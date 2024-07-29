7f2177d4765937b3c072ee96e3e5a034b01b9dc3
Junaid Syed <sjunaid626@gmail.com>
2024-07-29 20:46:22 +0530

    Add new projects

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,7 +4,10 @@ import { RiMailSendLine } from 'react-icons/ri';
 import { TbBrandGithubFilled } from 'react-icons/tb';
 
 const Homepage = () => (
-  <section id="home" className="bg-home-bg-mobile bg-white bg-no-repeat bg-contain flex flex-col items-center sm:items-start justify-center px-8 sm:p-48  sm:bg-home-bg-desktop md:w-full md:bg-contain md:bg-no-repeat rounded-bl-[100px] bg-center h-screen">
+  <section
+    id="home"
+    className="bg-home-bg-mobile bg-white bg-no-repeat bg-contain flex flex-col items-center sm:items-start justify-center px-8 sm:p-48  sm:bg-home-bg-desktop md:w-full md:bg-contain md:bg-no-repeat rounded-bl-[100px] bg-center h-screen"
+  >
     <div className="gap-4 flex flex-col">
       <h1 className="text-primary font-bold text-40 leading-52">
         I&apos;m Junaid.
@@ -12,21 +15,59 @@ const Homepage = () => (
         Glad to see you!
       </h1>
       <p className="text-primary font-normal text-base leading-6">
-        I&apos;m an experienced software developer with a degree in Computer Science Engineering.
+        I&apos;m an experienced software developer with a degree in Computer
+        Science Engineering.
         <br />
-        Bringing ideas to life through code. So if you have an idea that needs to be designed and
+        Passionate about crafting dynamic and responsive web applications. With
+        expertise in React, Next.js, and Ruby on Rails,
         <br />
-        coded, feel free to contact me.
+        I bring ideas to life from concept to deployment. Let&apos;s
+        collaborate to turn your vision into a digital reality!
       </p>
       <p className="text-tertiary font-medium text-base leading-6">
         LET&apos;S CONNECT
       </p>
       <div className="text-quaternary flex gap-2">
-        <a href="https://twitter.com/junaidahmedsd" aria-label="Twitter" target="_blank" rel="noreferrer"><FaTwitter className="hover:text-sky-400 hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" /></a>
-        <a href="https://www.linkedin.com/in/junaidahmedsyed/" aria-label="LinkedIn" target="_blank" rel="noreferrer"><FaLinkedin className="hover:text-[#0a66c2] hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" /></a>
-        <a href="mailto:sjunaid626@gmail.com" aria-label="Mail" target="_blank" rel="noreferrer"><RiMailSendLine className="hover:text-red-400 hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" /></a>
-        <a href="https://github.com/iamsjunaid" aria-label="GitHub" target="_blank" rel="noreferrer"><TbBrandGithubFilled className="hover:text-black hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" /></a>
-        <a href="https://wellfound.com/u/junaid-ahmed-shakeel-ahmed-syed" aria-label="Wellfound" target="_blank" rel="noreferrer"><FaAngellist className="hover:text-black hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" /></a>
+        <a
+          href="https://twitter.com/junaidahmedsd"
+          aria-label="Twitter"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaTwitter className="hover:text-sky-400 hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/junaidahmedsyed/"
+          aria-label="LinkedIn"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin className="hover:text-[#0a66c2] hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" />
+        </a>
+        <a
+          href="mailto:sjunaid626@gmail.com"
+          aria-label="Mail"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <RiMailSendLine className="hover:text-red-400 hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" />
+        </a>
+        <a
+          href="https://github.com/iamsjunaid"
+          aria-label="GitHub"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <TbBrandGithubFilled className="hover:text-black hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" />
+        </a>
+        <a
+          href="https://wellfound.com/u/junaid-ahmed-shakeel-ahmed-syed"
+          aria-label="Wellfound"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaAngellist className="hover:text-black hover:cursor-pointer text-2xl hover:cursor-pointer transition duration-500 ease-in-out" />
+        </a>
       </div>
     </div>
   </section>
